6718c57ec0b9865242fb46e6c9dbeb6e303703d1
rafy <jinhlee19@gmail.com>
2021-12-04 16:45:50 +0900

    I Am Rich

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text(
-            'hello World',
-          ), // Flutter doc recommend using comma, after ')'
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('I am rich'),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey.shade400,
+          ),
+          body: const Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ),
         ),
       ),
     );
