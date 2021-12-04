2eaf8efe0177d5f2bdfc84aa57db9a2a1b8a98ff
rafy <jinhlee19@gmail.com>
2021-12-04 16:55:43 +0900

    I Am Poor

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,
           appBar: AppBar(
             title: const Text('I am rich'),
             centerTitle: true,
-            backgroundColor: Colors.blueGrey.shade400,
+            backgroundColor: Colors.blueGrey.shade900,
           ),
           body: const Center(
             child: Image(
