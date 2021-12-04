9b234a8569d144680c9e268664ad3cb7a83385ea
rafy <jinhlee19@gmail.com>
2021-12-04 15:50:32 +0900

    Initial Commit

---
+++ i_am_rich/android/app/src/main/kotlin/co/kr/dshine/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.kr.dshine.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      const MaterialApp(
+        home: Center(
+          child: Text(
+            'hello World',
+          ), // Flutter doc recommend using comma, after ')'
+        ),
+      ),
+    );
