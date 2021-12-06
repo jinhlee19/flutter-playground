f5f1e0569c819bb9016e6e81d6c28a178ddbe4a3
rafy <jinhlee19@gmail.com>
2021-12-07 07:22:59 +0900

    Card & ListTile Widgets End - ListTile

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,96 +10,94 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue.shade700,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            CircleAvatar(
-              radius: 60,
-              backgroundColor: Colors.yellow,
-              // backgroundImage: NetworkImage("https://via.placeholder.com/100"),
-              backgroundImage: AssetImage('images/avatar.png'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "이진혁",
-              style: TextStyle(
-                fontFamily: 'Cafe24SsurroundAir',
-                fontSize: 30,
-                color: Colors.white,
-              ), // Text Style
-            ),
-            SizedBox(
-              height: 3,
-            ), // Text
-            Text(
-              "플러터 & 리액트 공부중",
-              style: TextStyle(
-                fontFamily: 'Cafe24SsurroundAir',
-                fontSize: 18,
-                color: Colors.blue.shade100,
-              ), // Text Style
-            ),
-            Text(
-              "함께가실 파티원 구합니다...",
-              style: TextStyle(
-                fontFamily: 'Cafe24SsurroundAir',
-                fontSize: 12,
-                color: Colors.blue.shade50,
-              ), // Text Style
-            ), // Text
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.fromLTRB(40, 20, 40, 5),
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.blue.shade700,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "010-8697-2046",
-                      style: TextStyle(
-                        color: Colors.blue.shade900,
-                        fontFamily: 'Cafe24SsurroundAir',
-                        fontSize: 15,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Colors.yellow,
+                    // backgroundImage: NetworkImage("https://via.placeholder.com/100"),
+                    backgroundImage: AssetImage('images/avatar.png'),
+                  ), // CircleAvatar
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "이진혁",
+                    style: TextStyle(
+                      fontFamily: 'Cafe24SsurroundAir',
+                      fontSize: 30,
+                      color: Colors.white,
+                    ), // Text Style
+                  ), // Text
+                  SizedBox(
+                    height: 3,
+                  ), // SizedBox
+                  Text(
+                    "플러터 & 리액트 공부중",
+                    style: TextStyle(
+                      fontFamily: 'Cafe24SsurroundAir',
+                      fontSize: 18,
+                      color: Colors.blue.shade100,
+                    ), // TextStyle
+                  ), // Text
+                  Text(
+                    "함께가실 파티원 구합니다...",
+                    style: TextStyle(
+                      fontFamily: 'Cafe24SsurroundAir',
+                      fontSize: 12,
+                      color: Colors.blue.shade50,
+                    ), // TextStyle
+                  ), // Text
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 20.0,
+                      width: 150,
+                      child: Divider(
+                        color: Colors.blue.shade50,
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      color: Colors.blue.shade900,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "jinhlee19@gmail.com",
-                      style: TextStyle(
+                  ),
+                  Card(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.blue.shade700,
+                      ), // Icon
+                      title: Text(
+                        "010-8697-2046",
+                        style: TextStyle(
+                          color: Colors.blue.shade900,
+                          fontFamily: 'Cafe24SsurroundAir',
+                          fontSize: 15,
+                        ), // TextStyle
+                      ), // Text
+                    ), //List Tile
+                  ), // Card
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
                         color: Colors.blue.shade900,
-                        fontFamily: 'Cafe24SsurroundAir',
-                        fontSize: 15,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ]), // Widget, Column
+                      ), // Icon
+                      title: Text(
+                        "jinhlee19@gmail.com",
+                        style: TextStyle(
+                          color: Colors.blue.shade900,
+                          fontFamily: 'Cafe24SsurroundAir',
+                          fontSize: 15,
+                        ), //TextStyle
+                      ), // Text
+                    ), //ListTile
+                  ), //Card
+                ]),
+          ), // Widget, Column
         ), // SafeArea
       ), // Scaffold
     ); // Material App
