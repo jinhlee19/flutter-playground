8b56743b41a90c9454cba3b58409f98560a94346
rafy <jinhlee19@gmail.com>
2021-12-07 06:02:00 +0900

    Material Icons with the Icon Widget

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -8,25 +8,90 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue.shade700,
         body: SafeArea(
           child: Column(children: <Widget>[
             CircleAvatar(
-              radius: 50,
+              radius: 60,
               backgroundColor: Colors.yellow,
               // backgroundImage: NetworkImage("https://via.placeholder.com/100"),
               backgroundImage: AssetImage('images/avatar.png'),
             ),
             Text(
-              "Jinhyuk Lee",
+              "이진혁",
               style: TextStyle(
-                fontSize: 20,
+                fontFamily: 'Cafe24SsurroundAir',
+                fontSize: 30,
                 color: Colors.white,
+              ), // Text Style
+            ), // Text
+            Text(
+              "플러터 & 리액트 공부중",
+              style: TextStyle(
+                fontFamily: 'Cafe24SsurroundAir',
+                fontSize: 18,
+                color: Colors.blue.shade100,
+              ), // Text Style
+            ),
+            Text(
+              "함께가실 파티원 구합니다...",
+              style: TextStyle(
+                fontFamily: 'Cafe24SsurroundAir',
+                fontSize: 12,
+                color: Colors.blue.shade50,
+              ), // Text Style
+            ), // Text
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.fromLTRB(40, 20, 40, 5),
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                    color: Colors.blue.shade700,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "010-8697-2046",
+                    style: TextStyle(
+                      color: Colors.blue.shade900,
+                      fontFamily: 'Cafe24SsurroundAir',
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ]),
-        ),
-      ),
-    );
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.blue.shade900,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "jinhlee19@gmail.com",
+                    style: TextStyle(
+                      color: Colors.blue.shade900,
+                      fontFamily: 'Cafe24SsurroundAir',
+                      fontSize: 15,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ]), // Widget, Column
+        ), // SafeArea
+      ), // Scaffold
+    ); // Material App
   }
 }
