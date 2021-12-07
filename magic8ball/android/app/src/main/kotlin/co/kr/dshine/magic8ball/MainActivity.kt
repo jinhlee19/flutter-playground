fcc84a1a5317a8bbf6eb79d963e3e6be51d55f63
rafy <jinhlee19@gmail.com>
2021-12-08 07:50:49 +0900

    Magic 8 Ball - Step 1

---
+++ magic8ball/android/app/src/main/kotlin/co/kr/dshine/magic8ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.kr.dshine.magic8ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ magic8ball/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue.shade700,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: BallPage(),
+        ),
+      ),
+    );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
