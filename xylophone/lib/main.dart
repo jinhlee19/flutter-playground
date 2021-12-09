07a2cf2a634b0c18807d6c3c65dc4e906981aae8
rafy <jinhlee19@gmail.com>
2021-12-09 09:41:38 +0900

    Xylophone - Customizing 2

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -8,6 +8,29 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              animalFriends(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class animalFriends extends StatefulWidget {
+  @override
+  _animalFriendsState createState() => _animalFriendsState();
+}
+
+class _animalFriendsState extends State<animalFriends> {
   // 빌드의 바깥에서 함수 생성**
   void playSound(int soundNumber) {
     // 함수 안에 input arguments** 넣고, 데이터 타입을 지정*.
@@ -35,46 +58,29 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: () {},
-                child: Image.asset('images/1.png'),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
-                child: Row(
-                  children: <Widget>[
-                    buildKey(color: Colors.red, soundNumber: 1),
-                    buildKey(color: Colors.orange, soundNumber: 2),
-                    buildKey(color: Colors.yellow, soundNumber: 3),
-                    buildKey(color: Colors.green, soundNumber: 4),
-                    buildKey(color: Colors.blue, soundNumber: 5),
-                    buildKey(color: Colors.indigo, soundNumber: 6),
-                    buildKey(color: Colors.purple, soundNumber: 7),
-                  ],
-                ),
-              ),
+    return Column(
+      children: [
+        TextButton(
+          onPressed: () {
+            buildKey();
+          },
+          child: Image.asset('images/$imageNumber.png'),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Row(
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.blue, soundNumber: 5),
+              buildKey(color: Colors.indigo, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
             ],
           ),
         ),
-      ),
+      ],
     );
-  }
-}
-
-class animalFriends extends StatefulWidget {
-  @override
-  _animalFriendsState createState() => _animalFriendsState();
-}
-
-class _animalFriendsState extends State<animalFriends> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
