88897534fb8a7a2b3f868235227947d04d927e4c
rafy <jinhlee19@gmail.com>
2021-12-11 03:43:36 +0900

    Quizzler - Displaying the Questions

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -27,29 +27,16 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   // scoreKeeper - Variable-type<Data-type>
 
-  List<Icon> scoreKeeper = [
-    // 수업과 달리 'const'를 붙여야 에러가 안남. 왜?**
-    const Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'Approximately one quarter of human bones are in the feet.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -57,15 +44,16 @@ class _QuizPageState extends State<QuizPage> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                // 'testing text',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -84,13 +72,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               child: const Text('True'),
               onPressed: () {
+                print(questionNumber);
                 setState(() {
-                  scoreKeeper.add(
-                    const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ), // Icon
-                  ); // add(); Ends
+                  questionNumber++;
+                  // scoreKeeper.add(
+                  //   const Icon(
+                  //     Icons.check,
+                  //     color: Colors.green,
+                  //   ), // Icon
+                  // ); // add(); Ends
                 }); //The user picked true.
               },
             ),
@@ -106,7 +96,12 @@ class _QuizPageState extends State<QuizPage> {
                 textStyle: const TextStyle(fontSize: 20.0),
               ),
               child: const Text('False'),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  questionNumber++; // rebuild 를 일으켜서 52열의 text 해당부분 questions[questionNumber]로 업데이트 시킨다.
+                });
+                print(questionNumber);
+              },
             ),
           ),
         ),
