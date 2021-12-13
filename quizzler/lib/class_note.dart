63ba971ce4af4edc1df65a6ac424ced62d4471a6
rafy <jinhlee19@gmail.com>
2021-12-13 09:12:22 +0900

    Quizzler - Polymorphism

--- quizzler/lib/class_note.dart
+++ quizzler/lib/class_note.dart
@@ -1,13 +1,21 @@
 void main() {
 // #1
-//   Car myNormalCar = Car();
-//   print(myNormalCar.numberOfSeats);
-//   myNormalCar.drive();
+  Car myNormalCar = Car();
+  print(myNormalCar.numberOfSeats);
+  myNormalCar.drive();
 
-  // #2-2
+// #2-2 Inheritance
   ElectricCar myTesla = ElectricCar();
   myTesla.drive();
   myTesla.recharge();
+
+// #3-2 Polymorphism 1
+  LevitatingCar myMagLev = LevitatingCar();
+  myMagLev.drive();
+
+// #4-2 Polymorphism 2
+  SelfDrivingCar myWaymo = SelfDrivingCar('1 Hacker Way');
+  myWaymo.drive();
 }
 
 // #1
@@ -23,5 +31,28 @@ class ElectricCar extends Car {
   int batteryLevel = 100;
   void recharge() {
     batteryLevel = 100;
+  }
+}
+
+// #3-1 Polymorphism - override a parent behavior for own custom version.
+class LevitatingCar extends Car {
+  @override
+  void drive() {
+    print('glide forward');
+  }
+}
+
+// #4-1 Polymorphism - override a parent behavior for own custom version.
+class SelfDrivingCar extends Car {
+  late String destination;
+  SelfDrivingCar(String userSetDestination) {
+    destination = userSetDestination;
+  }
+  @override
+  void drive() {
+    super.drive(); // Getting method from parents.
+
+    print('sterring towards $destination');
+    // overrides with a new custom version of print.
   }
 }
