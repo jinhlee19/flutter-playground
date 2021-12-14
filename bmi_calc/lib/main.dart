99fa724ddc0e3ee4f4c841f1bb7b392614b56156
rafy <jinhlee19@gmail.com>
2021-12-15 08:25:48 +0900

    BMI Calc - Flutter Themes 1

--- bmi_calc/lib/main.dart
+++ bmi_calc/lib/main.dart
@@ -6,6 +6,18 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: const ColorScheme.dark().copyWith(
+          primary: const Color(0xFF0A0E21),
+        ),
+        // secondary: Colors.yellow,
+        // out of the copyWith Method
+        // Q: 왜 copyWith 안으로 집어 넣을 수 없는지, 또는 copyWith 내부 대체 property
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
+        scaffoldBackgroundColor: const Color(0xFF090C22),
+      ),
       home: InputPage(),
     );
   }
@@ -26,9 +38,23 @@ class _InputPageState extends State<InputPage> {
       body: const Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+      // ** depreciated version
+      // floatingActionButton: Theme(
+      //   data: ThemeData(accentColor: Colors.yellow),
+      //   child: FloatingActionButton(
+      //     onPressed: () {},
+      //     child: const Icon(Icons.add),
+      //   ),
+      // ),
+      // ** new version (written in 12/15/2021)
+      // floatingActionButton: Theme(
+      //   data: ThemeData(
+      //       colorScheme:
+      //           ColorScheme.fromSwatch().copyWith(secondary: Colors.yellow)),
+      //   child: FloatingActionButton(
+      //     onPressed: () {},
+      //     child: const Icon(Icons.add),
+      //   ),
       ),
     );
   }
