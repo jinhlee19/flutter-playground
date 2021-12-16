bb37df95cd07334acbc72e17e13e33623b613140
rafy <jinhlee19@gmail.com>
2021-12-16 19:28:22 +0900

    BMI Calc - Customizing Slider Widget with Themes 1

--- bmi_calc/lib/input_page.dart
+++ bmi_calc/lib/input_page.dart
@@ -66,7 +66,7 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: ReusableCard(
               colour: kActiveCardColor,
-              //Todo # 9-1,4 Column 생성, 텍스트 스타일,
+              //Todo # 9-1,4 Column 생성, 텍스트 스타일, 7 정렬
               cardChild: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -75,6 +75,7 @@ class _InputPageState extends State<InputPage> {
                     style: kLabelTextStyle,
                   ),
                   Row(
+                    //Todo # 9-7 정렬
                     textBaseline: TextBaseline.alphabetic,
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.baseline,
@@ -85,17 +86,30 @@ class _InputPageState extends State<InputPage> {
                       Text('cm', style: kLabelTextStyle),
                     ],
                   ),
-                  Slider(
-                    value: height.toDouble(),
-                    min: 50.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        height = newValue.round();
-                      });
-                    }, // Slider Widget은 double로 데이터를 받기 때문에 toString()으로 컨버팅.
+                  SliderTheme(
+                    // Todo #9-10
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
+                      //기본값이 더 작아 보이지 않으므로 키우기
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                    ),
+                    child: Slider(
+                      // Todo # 9-8-2
+                      value: height.toDouble(),
+                      min: 50.0,
+                      max: 220.0,
+                      // activeColor: Colors.white, // overrides Theme ThumbColor
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          //Todo # 9-9
+                          height = newValue.round();
+                        });
+                      }, // Slider Widget은 double로 데이터를 받기 때문에 toString()으로 컨버팅.
+                    ),
                   ),
                 ],
               ),
