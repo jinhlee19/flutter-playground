4388518ae834ed498a92d77120e810b90e6933a7
rafy <jinhlee19@gmail.com>
2021-12-28 03:24:43 +0900

    Clima - Exception Handling and Null Aware Operations 3

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -15,13 +15,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    await Geolocator.requestPermission();
-    LocationPermission permission = await Geolocator.checkPermission();
-    // data type position 에 position 으로 사용.
-    // keyword 인 await 으로 geolocator 에서 현재 포지션을 찾는데 사용한다. input 에는 얼만큼 정확히 측정할 것인지를 포함한다.
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    try {
+      LocationPermission permission = await Geolocator.checkPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+      await Geolocator.requestPermission();
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
