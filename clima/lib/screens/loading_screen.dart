4ff58140f84d0b95592dcaca5d4f9f96c6b385c9
rafy <jinhlee19@gmail.com>
2021-12-27 18:50:34 +0900

    Clima - Starter Code for Async Await Demo

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -9,10 +9,12 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     await Geolocator.requestPermission();
+    LocationPermission permission = await Geolocator.checkPermission();
+    // data type position 에 position 으로 사용.
+    // keyword 인 await 으로 geolocator 에서 현재 포지션을 찾는데 사용한다. input 에는 얼만큼 정확히 측정할 것인지를 포함한다.
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
     print(position);
-    LocationPermission permission = await Geolocator.checkPermission();
   }
 
   @override
