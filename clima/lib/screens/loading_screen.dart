b6c78f73af81efb5ef8800327aaa49b3d589e4b0
rafy <jinhlee19@gmail.com>
2021-12-28 19:13:06 +0900

    Clima - Getting Actual API Data 1 - Err

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -4,41 +4,41 @@ import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
 
+const apiKey = '{APIKEY}';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
   @override
   void initState() {
     super.initState();
     print('initState initiated.');
-    // getLocation();
+    getLocation();
     getData();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
   }
 
   void getData() async {
     const uri =
-        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API_KEY}';
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
     http.Response response = await http.get(Uri.parse(uri));
-    // print(response.statusCode);
+
     if (response.statusCode == 200) {
       String data = response.body;
-      // print(data);
-      // var longitude = jsonDecode(data)['coord']['lon'];
-      // print(longitude);
-      // var weather = jsonDecode(data)['weather'][0]['description'];
-      // print(weather);
 
-      // 처리 될때까지 데이터타입을 알수 없으므로 dynamic datatype으로 유지.
       var decodeData = jsonDecode(data);
 
       late double temp = decodeData['main']['temp'];
