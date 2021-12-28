effdaa4ad1126f1e6783b6fcf285066169960c1b
rafy <jinhlee19@gmail.com>
2021-12-28 16:14:30 +0900

    Clima - API and HTTP Package

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,9 +12,10 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    getLocation();
+    print('initState initiated.');
+    // getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -19,6 +23,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    const uri =
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API_KEY}';
+    http.Response response = await http.get(Uri.parse(uri));
+    // print(response.statusCode);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+      var longitude = jsonDecode(data)['coord']['lon'];
+      print(longitude);
+      var weather = jsonDecode(data)['weather'][0]['description'];
+      print(weather);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
